02793938bb726b4b646dc9499e58948b56d82a44
Kamye <andrej15ganzha@gmail.com>
2017-03-10 21:38:44 +0200

    static version

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+$('.present__list').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1
+});
+
+$('.products__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    focusOnSelect: true,
+    arrows: true
+});
